f5f365f2f8015ed2ffb92d4e9cb8e62e25ddb591
Michael Redruello <redruellomichael@gmail.com>
2021-11-26 12:38:19 +0100

    Update Song.jsx

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Heart, HeartFill } from 'react-bootstrap-icons'
+
 
 const Song = ({ track }) => (
   <div className="py-3 trackHover">
@@ -10,6 +12,7 @@ const Song = ({ track }) => (
       {parseInt(track.duration) % 60 < 10
         ? "0" + (parseInt(track.duration) % 60)
         : parseInt(track.duration) % 60}
+        <Heart className="ml-2" color="white" />
     </small>
   </div>
 );
