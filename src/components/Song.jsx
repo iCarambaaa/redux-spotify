433a9a8caf70fe1196aeec77ddf4735b025e476d
Michael Redruello <redruellomichael@gmail.com>
2021-11-26 15:47:51 +0100

    Update

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 import { Heart, HeartFill } from 'react-bootstrap-icons'
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state) => ({
+  // liked: state.likedSongs.liked,
+  arrayOfSongs: state.arrayOfSongs.songs
+  // albumSongs: state.selectedAlbums.songs,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  // getAlbumByAlbumId: (albumId) => {
+  //   dispatch(albumSongByAlbumidAction(albumId));
+  // },
+});
 
 
-const Song = ({ track }) => (
-  <div className="py-3 trackHover">
-    <span className="card-title trackHover px-3" style={{ color: "white" }}>
-      {track.title}
-    </span>
-    <small className="duration" style={{ color: "white" }}>
-      {Math.floor(parseInt(track.duration) / 60)}:
-      {parseInt(track.duration) % 60 < 10
-        ? "0" + (parseInt(track.duration) % 60)
-        : parseInt(track.duration) % 60}
-        <Heart className="ml-2" color="white" />
-    </small>
-  </div>
-);
+const Song = ({ track, arrayOfSongs }) => {
 
-export default Song;
+  const likedBoolean = arrayOfSongs.includes(track.id)
+
+  // const handleClick = () => {
+  //   <HeartFill />
+  // }
+
+  return (
+    <div className="py-3 trackHover">
+      <span className="card-title trackHover px-3" style={{ color: "white" }}>
+        {track.title}
+      </span>
+      <small className="duration" style={{ color: "white" }}>
+        {Math.floor(parseInt(track.duration) / 60)}:
+        {parseInt(track.duration) % 60 < 10
+          ? "0" + (parseInt(track.duration) % 60)
+          : parseInt(track.duration) % 60}
+          {likedBoolean !== true ? <Heart className="ml-2" color="white" /> : <HeartFill className="ml-2" color="white" />}
+      </small>
+    </div>
+  )
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Song)
